f1827f81543e321a18f5510e04a6227901eb5d83
urabexon <hiroki708.a48uk@gmail.com>
2026-05-31 08:09:59 +0900

    fix: use relative paths in main.tsx and verify i18n in App

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "src/index.css";
-import "src/i18n";
-import App from "src/App.tsx";
+import "./index.css";
+import "./i18n";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
