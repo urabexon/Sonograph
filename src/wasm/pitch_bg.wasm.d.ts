9c8a73ca95d9a2e26e03856df95cb60b5acfe883
urabexon <hiroki708.a48uk@gmail.com>
2026-06-10 22:21:10 +0900

    build: add compiled wasm pitch module

---
+++ src/wasm/pitch_bg.wasm.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const calculate_rms: (a: number, b: number) => number;
+export const detect_pitch: (a: number, b: number, c: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
